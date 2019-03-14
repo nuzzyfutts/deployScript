3582204a6846f75366a708b0070591b8aea3fbd7
Aimal Wajihuddin <aimal@wajihudd.in>
2019-03-13 20:19:55 -0400

    added error checking and colors

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
-echo -e "Howdy, what folder do you want to serve assets from?"
-echo -e -n "\033[36m>\033[0m "
-read foldrdir
+echo -e "\033[35mDirectory\033[0m (relative to working directory) of assets you want to serve?"
+echo -e -n "\033[36;1m>\033[35m "
+read folderDirectory
 
-echo -e "Okay, I'll read from \033[35m$foldrdir\033[0m. What do you want to name the container?"
-echo -e -n "\033[36m>\033[0m "
-read container
+if [ ! -d $folderDirectory ]
+then
+	echo -e "\033[31mERROR:\033[0m The directory you specified does not exist!"
+	exit 127
+fi
 
-echo -e "The container will be named \033[32m$container\033[0m. What frontend rule do you want to use for traefik?"
-echo -e -n "\033[36m>\033[0m "
-read urlrule
+echo -e "\033[0mOkay, I'll read from \033[35m$folderDirectory\033[0m."
+echo -e "What do you want to\033[32m name the container\033[0m?"
+echo -e -n "\033[36;1m>\033[32m "
+read containerName
 
-echo -e "Creating container \033[32m$container\033[0m from \033[35m$foldrdir\033[0m mapped to \033[33m$urlrule\033[0m..."
-return
-docker run --name $container -v $PWD/$foldrdir:/usr/share/nginx/html:ro -d -l "traefik.enable=true" -l "traefik.backend=$container-static" -l "traefik.frontend.rule=Host:$urlrule" --network traefik_proxy nginx
+if docker ps -a --format '{{.Names}}' | grep -Eq "^${containerName}\$"; 
+then
+	echo -e "\033[31mERROR:\033[0m A container already exists with that name!"
+	exit 1
+fi
+
+echo -e "\033[0mThe container will be named \033[32m$containerName\033[0m."
+echo -e "What \033[33;1;4mURL\033[0m do you want to serve this site at? (frontend rule for traefik)"
+echo -e -n "\033[36;1m> \033[33;1;4m"
+read frontendURL
+
+echo -e "\033[0m\033[34mCreating container...\033[0m"
+
+docker run --name $containerName -v $PWD/$folderDirectory:/usr/share/nginx/html:ro -d -l "traefik.enable=true" -l "traefik.backend=$containerName-static" -l "traefik.frontend.rule=Host:$frontendURL" --network traefik_proxy nginx
+
+echo -e "\033[32mCreation successful!\033[0m"
+
+echo -e "Asset Direcotry: \033[35;1m$folderDirectory\033[0m"
+echo -e "Container Name: \033[32;1m$containerName\033[0m"
+echo -e "URL: \033[33;1;4m$frontendURL\033[0m"
+
+exit 0
